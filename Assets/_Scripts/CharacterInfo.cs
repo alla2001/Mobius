bc9fd941ebc878397e8f76b11d4ffb38fab36c01
Phyvie <61946637+Phyvie@users.noreply.github.com>
2023-07-04 18:08:17 +0200

    merge, but idk what happens

---
+++ Assets/_Scripts/CharacterInfo.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInfo : MonoBehaviour
+{
+    public float timeBeforeDeath =  60f;
+    public float timeGainedFromItem = 20f;
+
+    public void Start()
+    {
+        Item.itemCollected.AddListener(() => { timeBeforeDeath += timeGainedFromItem; });
+  
+
+
+    }
+    private void Update()
+    {
+
+        timeBeforeDeath -= Time.deltaTime;
+        if (timeBeforeDeath <= 0)
+        {
+            Destroy(gameObject);
+
+        }
+    }
+
+
+
+}
