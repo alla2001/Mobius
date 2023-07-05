8487178aa3e88d43d752ca4c2f1530cc91292687
Ilia <75846379+Fabsoll@users.noreply.github.com>
2023-07-05 13:09:27 +0200

    Character placement and Ui

--- Assets/_Scripts/Managers/UIManager.cs
+++ Assets/_Scripts/Managers/UIManager.cs
@@ -5,24 +5,25 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] TMP_Text timerText;
-    int minutes;
-    int seconds;
-    string timerString;
+    [SerializeField] GameObject panelRefernce;
 
     // Start is called before the first frame update
     void Start()
     {
+        panelRefernce.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Update timer from game manager
-        minutes = (int)GameManager.Instance.timeLeft / 60;
-        seconds = (int)GameManager.Instance.timeLeft % 60;
-
-        timerString = $"{minutes}:{seconds}";
-        timerText.text = timerString;
+        if (GameManager.Instance.currentState == GameState.RewardMode)
+        {
+            panelRefernce.gameObject.SetActive(true);
+        }
+        else
+        {
+            panelRefernce.gameObject.SetActive(false);
+        }
     }
+
 }
