7ea8479002949898e96a6888fef1a22ad9d4b398
Ilia <75846379+Fabsoll@users.noreply.github.com>
2023-07-10 01:02:47 +0200

    Updated character, implemented animation

--- Assets/_Scripts/CharacterInfo.cs
+++ Assets/_Scripts/CharacterInfo.cs
@@ -1,6 +1,8 @@
+using Dreamteck.Splines;
 using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [SelectionBase]
@@ -10,8 +12,16 @@ public class CharacterInfo : MonoBehaviour
     public float timeGainedFromItem = 20f;
     public float lowHealthThreshhold=20f;
 
+    private Animator characterAnimator;
+    private SplineFollower splineFollower;
+    private bool isDead;
+    private float timeBeforeDespawn = 10f;
+    private float elapsedTime;
+
     public void Start()
     {
+        splineFollower = GetComponent<SplineFollower>();
+        characterAnimator = GetComponentInChildren<Animator>();
         emitter = AudioManager.instance.AddEventEmitterComponent(FMODEvents.instance.characterDeathWarning, this.gameObject);
     }
 
@@ -26,6 +36,10 @@ public class CharacterInfo : MonoBehaviour
     FMODUnity.StudioEventEmitter emitter;
     private void Update()
     {
+        characterAnimator.speed = 1 - Mathf.InverseLerp(60f, 0f, timeBeforeDeath);
+
+        Debug.Log("character anim speed: " + characterAnimator.speed);
+
         bool wentlow=false;
         if (timeBeforeDeath>lowHealthThreshhold)
         {
@@ -39,17 +53,24 @@ public class CharacterInfo : MonoBehaviour
         }
         if (timeBeforeDeath <= 0)
         {
+            splineFollower.followSpeed = 0;
             emitter.Stop();
             AudioManager.instance.PlayOneShot(FMODEvents.instance.characterDeath);
             if (tag == "Player")
             {
                 GameManager.Instance.ChangeState(GameState.GodView); 
             }
-            Destroy(gameObject); //should play deathAnimation here
+            characterAnimator.SetTrigger("isDead");
+            isDead = true;
+        }
 
+        if (isDead)
+        {
+            // SOUND LITERALLY CRASHED UNITY
+            /*elapsedTime += Time.deltaTime;
+            if (elapsedTime >= timeBeforeDespawn)*/
+            Destroy(gameObject);
         }
     }
-
-
 
 }
